974c5923f92aec1305822df4a8062108d85e43b9
Rommel Rico <rommeltj@gmail.com>
2013-01-30 19:02:55 -0800

    Chapters 1 through 4

---
+++ Ch04/GoodDogTestDrive.java
@@ -0,0 +1,12 @@
+class GoodDogTestDrive {
+    public static void main(String[] args) {
+	GoodDog one = new GoodDog();
+	GoodDog two = new GoodDog();
+	one.setSize(70);
+	two.setSize(8);
+	System.out.println("Dog one: " + one.getSize());
+	System.out.println("Dog two: " + two.getSize());
+	one.bark();
+	two.bark();
+    }
+}
