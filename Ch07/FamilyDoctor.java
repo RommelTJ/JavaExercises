2ed27893fef98d0309f43abf07ee4c26045c8ec4
Rommel Rico <rommeltj@gmail.com>
2013-02-02 10:14:57 -0800

    Chapter 7

---
+++ Ch07/FamilyDoctor.java
@@ -0,0 +1,8 @@
+public class FamilyDoctor extends Doctor {
+    boolean makesHouseCalls;
+
+    void giveAdvice() {
+	//give homespun advice.
+    }
+}
+
---
+++ Ch07/Surgeon.java
@@ -0,0 +1,11 @@
+public class Surgeon extends Doctor {
+    
+    void treatPatien() {
+	//perform surgery - overriden method
+    }
+
+    void makeIncision() {
+	//make incision
+    }
+}
+
