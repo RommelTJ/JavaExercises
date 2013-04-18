001cdf1f6fa680c391c4ae182adaf76c76eae0ae
Rommel Rico <rommeltj@gmail.com>
2013-04-18 07:32:31 -0700

    Ch11

--- Ch11/ExTestDrive.java
+++ Ch11/ExTestDrive.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class MyEx extends Exception {}
 
 public class ExTestDrive {
@@ -24,4 +25,32 @@ public class ExTestDrive {
 		System.out.print("r");
 		System.out.print("o");
 	}
+=======
+class MyEx extends Exception { }
+
+public class ExTestDrive {
+    public static void main(String[] args) {
+	String test = args[0];
+	System.out.print("t");
+	try {
+	    doRisky(test);
+	}
+	catch(MyEx e) {
+	    System.out.print("a");
+	}
+	finally{
+	    System.out.print("w");
+	    System.out.println("s");
+	}
+    }
+
+    static void doRisky(String t) throws MyEx {
+	System.out.print("h");
+	if ("yes".equals(t)) {
+	    throw new MyEx();
+	}
+	System.out.print("r");
+	System.out.print("o");
+    }
+>>>>>>> Ch11
 }
--- Ch11/MusicTest1.java
+++ Ch11/MusicTest1.java
@@ -1,6 +1,7 @@
 import javax.sound.midi.*;
 
 public class MusicTest1 {
+<<<<<<< HEAD
 	public void play() {
 		try {
 			Sequencer sequencer = MidiSystem.getSequencer();
@@ -16,4 +17,21 @@ public class MusicTest1 {
 		m1.play();
 	} //Close main
 } //Close class.
+=======
+    public void play() {
+	try {
+	    Sequencer sequencer = MidiSystem.getSequencer();
+	    System.out.println("We got a sequencer");
+	}
+	catch(MidiUnavailableException ex) {
+	    System.out.println("Bummer");
+	}
+    } //Close play
+
+    public static void main(String[] args) {
+	MusicTest1 mt = new MusicTest1();
+	mt.play();
+    } //Close main
+} //Close class
+>>>>>>> Ch11
 
