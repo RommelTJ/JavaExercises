b7e58adf1a663e1748beb5eda5e17bd24468b09a
Rommel Rico <rommeltj@gmail.com>
2013-04-24 22:21:13 -0700

    Ch15

---
+++ Ch15/ThreadTester.java
@@ -0,0 +1,8 @@
+class ThreadTester {
+	public static void main(String[] args) {
+		Runnable threadJob = new MyRunnable();
+		Thread myThread = new Thread(threadJob);
+		myThread.start();
+		System.out.println("Back in main.");
+	}
+}
